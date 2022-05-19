98137a19c3f91b4ba55d01352fef2c0b22e5b115
filip <fso@stromlin.com>
2022-05-19 18:33:43 +0200

    layout search interface and fix other pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ module.exports = {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        sub1: resolve(__dirname, "subpages/search-interface.html"),
+        sub1: resolve(__dirname, "subpages/search-page.html"),
         sub2: resolve(__dirname, "subpages/sign-up.html"),
       },
     },
