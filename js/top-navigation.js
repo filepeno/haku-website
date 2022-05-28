6db4f6a9ee086c4aba2cba216bf33644ea46d8b4
filip <fso@stromlin.com>
2022-05-28 13:50:55 +0200

    add nav functionality to display on click

--- js/top-navigation.js
+++ js/top-navigation.js
@@ -1,10 +1,29 @@
 import { HTML } from "../main";
 
+let navExpanded = false;
+
 export function initHeader() {
+  HTML.expandableNav = document.querySelector(".expandable-nav");
   HTML.toggleNavBtn = document.querySelector(".toggle-nav-btn");
-  HTML.toggleNavBtn.addEventListener("click", toggleNav);
+  HTML.toggleNavBtn.addEventListener("click", toggleBtn);
 }
 
-function toggleNav() {
+function toggleBtn() {
   console.log("click");
+  if (!navExpanded) {
+    navExpanded = true;
+    HTML.toggleNavBtn.classList.add("expandable-active");
+  } else {
+    navExpanded = false;
+    HTML.toggleNavBtn.classList.remove("expandable-active");
+  }
+  toggleExpandableNav();
+}
+
+function toggleExpandableNav() {
+  if (!navExpanded) {
+    HTML.expandableNav.classList.remove("active");
+  } else {
+    HTML.expandableNav.classList.add("active");
+  }
 }
